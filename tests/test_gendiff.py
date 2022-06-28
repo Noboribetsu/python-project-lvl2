01f1ab1d7b117dc5acda7f75c27ac024244cd025
Vova <bkj@ya.ru>
2022-06-28 15:21:43 +0300

    fix test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -14,33 +14,44 @@ def result(key):
     with open(RESULT[key]) as f:
         return f.read()
 
+
 def test_gendiff_json_stylish():
-    script_run = subprocess.run(['gendiff', json_file[0], json_file[1]], \
-                                stdout=subprocess.PIPE, encoding='utf-8')
+    script_run = subprocess.run(
+        ['gendiff', json_file[0], json_file[1]],
+        stdout=subprocess.PIPE, encoding='utf-8')
     assert script_run.stdout[:-1] == result('stylish')
 
 
 def test_gendiff_yaml_stylish():
-    script_run = subprocess.run(['gendiff', yaml_file[0], yaml_file[1]], \
-                                stdout=subprocess.PIPE, encoding='utf-8')
+    script_run = subprocess.run(
+        ['gendiff', yaml_file[0], yaml_file[1]],
+        stdout=subprocess.PIPE, encoding='utf-8')
     assert script_run.stdout[:-1] == result('stylish')
 
+
 def test_gendiff_json_plain():
-    script_run = subprocess.run(['gendiff','-f', 'plain', json_file[0], json_file[1]], \
-                                stdout=subprocess.PIPE, encoding='utf-8')
+    script_run = subprocess.run(
+        ['gendiff', '-f', 'plain', json_file[0],
+            json_file[1]], stdout=subprocess.PIPE, encoding='utf-8')
     assert script_run.stdout[:-1] == result('plain')
 
+
 def test_gendiff_yaml_plain():
-    script_run = subprocess.run(['gendiff','-f', 'plain', yaml_file[0], yaml_file[1]], \
-                                stdout=subprocess.PIPE, encoding='utf-8')
-    assert script_run.stdout[:-1] == result('plain')    
+    script_run = subprocess.run(
+        ['gendiff', '-f', 'plain', yaml_file[0], yaml_file[1]],
+        stdout=subprocess.PIPE, encoding='utf-8')
+    assert script_run.stdout[:-1] == result('plain')
+
 
 def test_gendiff_json_to_json():
-    script_run = subprocess.run(['gendiff','-f', 'json', json_file[0], json_file[1]], \
-                                stdout=subprocess.PIPE, encoding='utf-8')
+    script_run = subprocess.run(
+        ['gendiff', '-f', 'json', json_file[0], json_file[1]],
+        stdout=subprocess.PIPE, encoding='utf-8')
     assert script_run.stdout[:-1] == result('json')
 
+
 def test_gendiff_yaml_to_json():
-    script_run = subprocess.run(['gendiff','-f', 'json', yaml_file[0], yaml_file[1]], \
-                                stdout=subprocess.PIPE, encoding='utf-8')
-    assert script_run.stdout[:-1] == result('json')    
+    script_run = subprocess.run(
+        ['gendiff', '-f', 'json', yaml_file[0], yaml_file[1]],
+        stdout=subprocess.PIPE, encoding='utf-8')
+    assert script_run.stdout[:-1] == result('json')
